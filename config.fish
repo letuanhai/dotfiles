38a9f09392fa31af1946c97e8b70bc33b17bf46d
letuanhai <letuanhai@live.com>
2020-02-20 23:33:31 +0700

    add conda init to fish

--- config.fish
+++ config.fish
@@ -6,3 +6,9 @@ end
 
 # WSL X server setting
 export DISPLAY=:0
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /d/wsl/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
