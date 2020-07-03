0b31e029c6470e293283e7df0da47f6f850d8132
letuanhai <letuanhai@live.com>
2020-07-03 16:24:45 +0700

    stop tmux at login and update git alias

--- config.fish
+++ config.fish
@@ -1,11 +1,15 @@
-if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
+# Auto start tmux
+#if status is-interactive
+#and not set -q TMUX
+#    exec tmux
+#end
 
 # Aliases
 alias v='nvim'
+## git alias
+alias g='git'
 alias gs='git status'
-alias gck='git checkout'
 alias gcm='git commit'
+alias gck='git checkout'
+## reload config
 alias sf='source ~/.config/fish/config.fish'
