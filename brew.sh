1c6fab44473007f693a3d410fad52fc3ae42f6eb
letuanhai <letuanhai@live.com>
2020-11-26 15:06:45 +0700

    Install antigen and delete unused app

--- brew.sh
+++ brew.sh
@@ -48,38 +48,6 @@ brew install screen
 brew install php
 brew install gmp
 
-# Install font tools.
-#brew tap bramstein/webfonttools
-#brew install sfnt2woff
-#brew install sfnt2woff-zopfli
-#brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-#brew install aircrack-ng
-#brew install bfg
-#brew install binutils
-#brew install binwalk
-#brew install cifer
-#brew install dex2jar
-#brew install dns2tcp
-#brew install fcrackzip
-#brew install foremost
-#brew install hashpump
-#brew install hydra
-#brew install john
-#brew install knock
-#brew install netpbm
-#brew install nmap
-#brew install pngcheck
-#brew install socat
-#brew install sqlmap
-#brew install tcpflow
-#brew install tcpreplay
-#brew install tcptrace
-#brew install ucspi-tcp # `tcpserver` etc.
-#brew install xpdf
-#brew install xz
-
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
@@ -105,6 +73,7 @@ brew install jq
 brew cask install alacritty font-fira-code
 brew install tmux neovim reattach-to-user-namespace fasd fzf diff-so-fancy
 $(brew --prefix)/opt/fzf/install
+brew install antigen
 
 # Install Pyenv to manage Python version
 #brew install pyenv
